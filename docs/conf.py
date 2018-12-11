93fc4a3739cbeee3a951e85188f5fa2fd0ceafae
Volker Kempert <volker.kempert@almedso.de>
2018-12-11 17:19:06 +0100

    Switch to read the docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,6 @@
 
 import sys
 import os
-import sphinx_bootstrap_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -39,7 +38,6 @@ extensions = [
     'sphinx.ext.extlinks',
     'sphinxcontrib.bibtex',
     'sphinxcontrib.plantuml',
-    'sphinxcontrib.ansi',
     'sphinxcontrib.programoutput',
     'sphinxcontrib.email',
     'sphinxcontrib.blockdiag',
@@ -118,7 +116,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'bootstrap'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -126,7 +124,7 @@ html_theme = 'bootstrap'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+# html_theme_path = sphinx_rtd_theme.get_html_theme_path()
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
